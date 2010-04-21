c1e7df184e8d5271861d3d19a3bc028f7240e377
Jordi Funollet <jordi.f@ati.es>
2010-04-22 00:23:53 +0200

    added license

--- __init__.py
+++ __init__.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2010 Jordi Funollet <jordi.f@ati.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
 from fabric.api import *
 from fabric.contrib.files import *
 
@@ -134,7 +150,6 @@ Parameters:
 def new_db (mysql_db, local_user, mysql_client=None):
     """New Mysql DB with a new user/password.
 
-Put .my.cnf on <mysql_client>, at ~/.my.cnf.<mysql_db>
     
     mysql_db:       name of the new database
     local_user:     username at the server 
--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,9 @@
+# -*- coding: utf-8 -*-
+"""This file is just for lazyness, so we don't need to call Fabric with
+
+    $ fab -f __init__.py ...
+
+Defining tasks on __init__.py allows cleaner imports from other fabfiles.
+"""
+
 from __init__ import *
